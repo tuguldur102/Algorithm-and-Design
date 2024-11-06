740e7ab238a83fc2a09415f43d4e39145f2a5700
tuguldur102 <b.tuguldur102@gmail.com>
2024-11-06 14:29:36 +0800

    day2

--- NeetCode/Array/two_integer_sum.py
+++ NeetCode/Array/two_integer_sum.py
@@ -3,10 +3,14 @@ from typing import List
 class Solution:
   def two_int_sum(self, nums: List[int], target: int) -> List[int]:
 
-    result = 
+    result = dict() # val -> index
 
+    for i, num in enumerate(nums):
 
-    
+      diff = target - num
+      if diff in result:
+        return [result[diff], i]  
+      result[i] = num
   
 if __name__ == "__main__":
   s, t = input().split(" ")
